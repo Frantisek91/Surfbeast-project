2cc2ff38264f8a3c288e11400bb706de7f43bfd1
LukasMicka <lukas.micka@seznam.cz>
2019-03-18 16:01:38 +0100

    modified camps table

--- resources/views/camps/show.blade.php
+++ resources/views/camps/show.blade.php
@@ -4,58 +4,59 @@
     
 <div class="container">
 
-<h2>{{ $camp->agency->name }}</h2>
+    <h2>{{ $camp->agency->name }}</h2>
 
-<h3>{{ $camp->destination->name }}</h3>
+    <h3>{{ $camp->destination->name }}</h3>
+    <br>
 
-<h4>Popis</h4>
+    <h5>Popis</h5>
 
-<p>{{ $camp->description }}</p>
+    <p>{{ $camp->description }}</p>
 
-<h4>Stravování</h4>
+    <h5>Stravování</h5>
 
-<p>{{ $camp->catering }}</p>
+    <p>{{ $camp->catering }}</p>
 
-<h4>Doprava</h4>
+    <h5>Doprava</h5>
 
-<p>{{ $camp->transport }}</p>
+    <p>{{ $camp->transport }}</p>
 
-@if ($camp->reviews->count())
-    <h4>Komentáře</h4>
+    @if ($camp->reviews->count())
+        <h5>Komentáře</h5>
 
-    @foreach ($camp->reviews as $review)
-        <h5>Od: {{$review->user->name}}</h5>
-        <h5>Celkové hodnocení:{{$review->rating}}</h5>
-        <p>Komentář:{{$review->description}}</p>
-        
-    @endforeach  
+        @foreach ($camp->reviews as $review)
+            <h5>Od: {{$review->user->name}}</h5>
+            <h5>Celkové hodnocení:{{$review->rating}}</h5>
+            <p>Komentář:{{$review->description}}</p>
+            
+        @endforeach  
 
-@endif
+    @endif
 
-<form method="POST" action="{{action("ReviewsController@store", $camp->id)}}">
-    @csrf
+    <form method="POST" action="{{action("ReviewsController@store", $camp->id)}}">
+        @csrf
 
-    <div class="form-group">
-    <label>Overall Experience</label>
-    <select class="form-control" name="rating">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
-    </div>
-
-    <div class="form-group">
-    <textarea class="form-control" name="description" rows="3"></textarea>
-    </div>
-
-    <div class="form-group row">
-        <div class="col-sm-10">
-        <button type="submit" class="btn btn-primary">Share</button>
+        <div class="form-group">
+        <label>Overall Experience</label>
+        <select class="form-control" name="rating">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
         </div>
-    </div>
-</form>
 
-</div>
+        <div class="form-group">
+        <textarea class="form-control" name="description" rows="3"></textarea>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-10">
+            <button type="submit" class="btn btn-primary">Share</button>
+            </div>
+        </div>
+    </form>
+
+    </div>
 @endsection
