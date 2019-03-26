33ec78d6d7cdce0c7c97345398e0aa8aae294b1d
LukasMicka <lukas.micka@seznam.cz>
2019-03-26 11:08:24 +0100

    finished search show page

--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -20,52 +20,80 @@
 @endif
 
   <form action="{{ action('SurfController@show') }}" method="get">
-    <div class="form-row d-flex justify-content-center my-3">
-      <div class="col">
-        <select name="destination_id" class="form-control">
+    <div class="container">
+    <div class="form-row my-3">
+      <div class="col-12 col-md">
+        <select name="destination_id" class="form-control form-control-sm">
           @foreach($destinations as $d)
             <option value="{{ $d->id }}" {{ $d->id == $destination->id ? "selected" : "" }}>{{ $d->name }}</option>
           @endforeach
         </select>
       </div>
-      <div class="col">
-        <input type="date" name="start" class="form-control" placeholder="Od kdy?" value="{{ $start }}"/>
+      <div class="col-6 col-md">
+        <input type="date" name="start" class="form-control form-control-sm" placeholder="Od kdy?" value="{{ $start }}"/>
       </div>
-      <div class="col">
-        <input type="date" name="end" class="form-control" placeholder="Do kdy?" value="{{ $end }}"/>
+      <div class="col-6 col-md">
+        <input type="date" name="end" class="form-control form-control-sm" placeholder="Do kdy?" value="{{ $end }}"/>
       </div>
-      <div class="col">
-        <input type="number" name="price_min" class="form-control" placeholder="Částka od" value="{{ $price_min }}"/>
+      <div class="col-6 col-md">
+        <input type="number" name="price_min" class="form-control form-control-sm" placeholder="Částka od" value="{{ $price_min }}"/>
       </div>
-      <div class="col">
-        <input type="number" name="price_max" class="form-control" placeholder="do" value="{{ $price_max }}"/>
+      <div class="col-6 col-md">
+        <input type="number" name="price_max" class="form-control form-control-sm" placeholder="do" value="{{ $price_max }}"/>
       </div>
-      <div class="col">
-        <select name="sort" id="" class="form-control">
-          <option value="start-asc" {{ $sort == "start-asc" ? "selected" : "" }}>Od nejbližšího</option>
-          <option value="start-desc" {{ $sort == "start-desc" ? "selected" : "" }}>Od nejpozdějšího</option>
+      <div class="col-12 col-md">
+        <select name="sort" id="" class="form-control form-control-sm">
+          <option value="start-asc" {{ $sort == "start-asc" ? "selected" : "" }}>Nejbližší</option>
+          <option value="start-desc" {{ $sort == "start-desc" ? "selected" : "" }}>Nejpozdější</option>
         </select>
       </div>
-      <div class="col">
-        <button type="submit" class="btn btn-primary" class="form-control">Uprav vyhledávání</button>
+      <div class="col-12 col-md">
+        <button type="submit" class="btn btn-primary btn-sm">Hledej</button>
       </div>
     </div>
+  </div>
   </form>
 
     @if(!empty($camps))
-        @foreach ($camps as $camp)     
+    <div class="row">
+
+      
+        @foreach ($camps as $camp)  
+        <div class="col-12 col-md-4">
+   
             @if(!empty($camp->terms))
+
+
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                  <img src="{{ $camp->image_url_1 }}" class="card-img" alt="Surfcamp">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $camp->name }}</h5>
+
                 <a href="{{ action('AgencyController@show', $camp->agency_id) }}" target="_blank"><h5>{{ $camp->agency->name }}</h5></a>
-                    <ul>
-                        @foreach ($camp->terms as $term)
-                            <li>Od: {{ $term->start }} Do: {{ $term->end }} Cena: {{ $term->price }}</li>
+                        {{-- @foreach ($camp->terms as $term)
+                          <div>Od: {{ $term->start }} Do: {{ $term->end }}</div>    
+                          <br>
+                          <div>Cena: {{ $term->price }}</div>
                         @endforeach
-                    </ul>
+                        <br> --}}
                 <a href="{{ action('CampController@show', $camp->id) }}" target="_blank" class="btn btn-primary">Zobrazit detaily</a>
                 <br>
                 <br>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
             @endif
+          </div>
+
         @endforeach
+    </div>
     @endif
 
 @endsection
